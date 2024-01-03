a45a7c8c40e01f9871afd97e175aafbecaa74ca6
Priynshu <52713855+IZZO29@users.noreply.github.com>
2024-01-03 16:06:57 +0530

    SocialLinks.js

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -40,7 +40,7 @@ const SocialLinks = () => {
                 Resume <BsFillPersonLinesFill size={30} /> 
                 </>
             ),
-            href: '/Myresume.pdf',
+            href: '/resume_p.pdf',
             download: true
         }
     ]
